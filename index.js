ab7e4a2c2b913d13d0e491a414e59563ebab389f
hanzala bawany <hanzalabawany0@gmail.com>
2025-08-15 11:14:41 +0500

    changes for vercel dploetyemnet

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ app.get('/', (req, res) => {
 })
 
 
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
 }
 
+export default app;
 
-// export default app;
