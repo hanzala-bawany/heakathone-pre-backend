dff5e33144d3138dd23206bb159658dae5cc2735
hanzala bawany <hanzalabawany0@gmail.com>
2025-08-22 10:28:56 +0500

    changes

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -3,7 +3,8 @@ import { successHandler } from "../utills/resHandler.js";
 
 export const testGetDatController = (req,res) =>{
     try{
-        console.log(req,"---> req");
+        // console.log(req,"---> req");
+        // console.log(req?.query,"--> query params");
         // console.log(req.headers.authorization,"-----> token");
         successHandler(res,200,`test data return successfully`, {title : "test data" , quantity : 222} )
     }
