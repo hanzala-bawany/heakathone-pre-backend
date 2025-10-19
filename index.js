84e125c8c08f4db50d85a0ab845320505afead3d
hanzala bawany <hanzalabawany0@gmail.com>
2025-10-19 05:35:56 +0500

    pre work

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ connectDB()
 // app.use(helmet());
 // app.use(mongoSanitize());
 app.use(cors({
- origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"] ,
-//  credentials: true
-}))
+  origin: "*", // sab domains allow
+}));
+
 app.use(express.json())
 // app.use(cookieParser())
 
